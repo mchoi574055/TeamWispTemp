e50795cbb30654552d44bfde3d610ef1d5cc5020
futoidaddy <57603310+futoidaddy@users.noreply.github.com>
2022-01-26 14:43:41 -0800

    half functioning pause menu added

--- TeamWisp/Assets/MainMenu.cs
+++ TeamWisp/Assets/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
     public void PlayGame () {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //game goes straight to pause menu and idk why
     }
 
     public void QuitGame () {
