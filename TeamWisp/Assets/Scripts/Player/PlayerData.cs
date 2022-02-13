06c854408279d3e97965f7640bc0452edfabe038
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2022-02-13 13:13:07 -0800

    cleaning

--- TeamWisp/Assets/Scripts/Player/PlayerData.cs
+++ TeamWisp/Assets/Scripts/Player/PlayerData.cs
@@ -7,18 +7,14 @@ public class PlayerData
 {
     public int level;
     public int health;
-    public float[] position;
+    public Vector3 position;
 
     public PlayerData(Player player)
     {
         level = player.level;
         // health = player.health;
-
-
-        position = new float[3];
-        position[0] = player.transform.position.x;
-        position[1] = player.transform.position.y;
-        position[2] = player.transform.position.z;
+        
+        position = player.transform.position;
 
     }
 }
