a7cdb5bbd9b67abaf09d8c9690f34c451cc71619
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2022-02-21 14:30:28 -0800

    followpath stuff

--- TeamWisp/Assets/Scripts/Hero/StateMachine/FollowPath.cs
+++ TeamWisp/Assets/Scripts/Hero/StateMachine/FollowPath.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class FollowPath : StateMachineBehaviour
 {
     private Hero.Movement.FollowPath followPath;
-    
+    private static readonly int velocityX = Animator.StringToHash("VelocityX");
+    private static readonly int velocityY = Animator.StringToHash("VelocityY");
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -15,8 +17,8 @@ public class FollowPath : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetFloat("VelocityX", followPath.direction.x);
-        animator.SetFloat("VelocityY", followPath.direction.y);
+        animator.SetFloat(velocityX, followPath.direction.x);
+        animator.SetFloat(velocityY, followPath.direction.y);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
