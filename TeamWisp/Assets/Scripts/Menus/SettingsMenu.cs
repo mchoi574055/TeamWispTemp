310cf4cb1dfc2ce52d026ab8b5afdd7f77d5e19b
Jeffrey <jeffreyshen859@gmail.com>
2022-02-23 22:03:41 -0800

    keep working, save point

--- TeamWisp/Assets/Scripts/Menus/SettingsMenu.cs
+++ TeamWisp/Assets/Scripts/Menus/SettingsMenu.cs
@@ -6,13 +6,14 @@ using UnityEngine.UI;
 public class SettingsMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] Slider volumeSlider;
-    [SerializeField] Toggle fullScreen;
-    Dropdown resolutionDropdown;
+    [SerializeField] Slider volSlider;
+    [SerializeField] Toggle fullScreenToggle;
+    [SerializeField] Dropdown resDropDown;
+
     void Start()
     {
-        volumeSlider.value = Settings.VOLUME;
         
+        loadSettings();
     }
 
     // Update is called once per frame
@@ -22,11 +23,12 @@ public class SettingsMenu : MonoBehaviour
     }
 
 
-    public void setVolume(Slider volume)
+
+    public void setVolume(System.Single vol)
     {
         // set game volume https://alessandrofama.com/tutorials/fmod/unity/mixer
-        Settings.VOLUME = volume.value;
-        Debug.Log(volume.value);
+        //Settings.VOLUME = volume.value;
+        Debug.Log((float)vol);
     }
 
     public void setResolution(int resolution)
@@ -34,10 +36,17 @@ public class SettingsMenu : MonoBehaviour
         Debug.Log(resolution);
     }
 
-    public void setFullScreen(Toggle fullscreen)
+    public void setFullScreen(bool fullScreen)
     {
-        Debug.Log(fullscreen.isOn);
+        Debug.Log(fullScreen);
         //Settings.isFullScreen = isOn ? 1 : 0;
         //Screen.fullScreen = isOn;
     }
+
+    public void loadSettings()
+    {
+        Debug.Log("hi");
+        volSlider.value = Settings.VOLUME;
+    }
+    
 }
