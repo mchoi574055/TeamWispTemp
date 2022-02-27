b95af56e569aeb65e9e60af7b5d8fd87fec69d15
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2022-02-27 14:41:29 -0800

    minor updates

--- TeamWisp/Assets/Scripts/Menus/MainMenu.cs
+++ TeamWisp/Assets/Scripts/Menus/MainMenu.cs
@@ -8,30 +8,6 @@ public class MainMenu : MonoBehaviour
 {
 
     [SerializeField] private string gameScene = "";
-    [SerializeField] GameObject play_button;
-    [SerializeField] GameObject option_button;
-    [SerializeField] GameObject quit_button;
-
-    [SerializeField] GameObject save_button1;
-    [SerializeField] GameObject save_button2;
-    [SerializeField] GameObject save_button3;
-    [SerializeField] GameObject save_button4;
-
-    public void PlayGame () {
-        play_button.SetActive(false);
-        option_button.SetActive(false);
-        quit_button.SetActive(false);
-
-        SaveMenu();
-    }
-
-    public void SaveMenu()
-    {
-        save_button1.SetActive(true);
-        save_button2.SetActive(true);
-        save_button3.SetActive(true);
-        save_button4.SetActive(true);
-    }
 
     public void QuitGame () {
         Debug.Log("QUIT!");
